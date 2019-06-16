b15f0d2dc9788369ae0ceb5c436d6dab731ac84c
Mark Sauter <dr.funk.mark@gmail.com>
2019-06-16 11:45:04 -0500

    feat(helpers.ts): overload helper connect to accept function type messages

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,13 +4,45 @@ import { Kelm, Update } from './state'
 import { Component } from './component'
 import { Node } from './node'
 
-export function connect<UPDATE extends Update>(
+type NodeEventMap = GlobalEventHandlersEventMap & {
+  [index: string]: any
+}
+
+export function connect<UPDATE extends Update, K extends keyof NodeEventMap>(
+  stream: flyd.Stream<UPDATE['_Msg']>,
+  node: konva.Node,
+  evtStr: K,
+  msg: UPDATE['_Msg']
+): void
+export function connect<UPDATE extends Update, K extends keyof NodeEventMap>(
+  stream: flyd.Stream<UPDATE['_Msg']>,
+  node: konva.Node,
+  evtStr: K,
+  msg: (e: konva.KonvaEventObject<NodeEventMap[K]>) => UPDATE['_Msg']
+): void
+export function connect<UPDATE extends Update, K extends keyof NodeEventMap>(
+  kelm: Kelm<UPDATE>,
+  node: konva.Node,
+  evtStr: K,
+  msg: UPDATE['_Msg']
+): void
+export function connect<UPDATE extends Update, K extends keyof NodeEventMap>(
   kelm: Kelm<UPDATE>,
   node: konva.Node,
   evtStr: string,
-  msg: UPDATE['_Msg']
-) {
-  node.on(evtStr, () => kelm.stream()(msg))
+  msg: (e: konva.KonvaEventObject<NodeEventMap[K]>) => UPDATE['_Msg']
+): void
+export function connect<UPDATE extends Update, K extends keyof NodeEventMap>(
+  kelmOrStream: flyd.Stream<UPDATE['_Msg']> | Kelm<UPDATE>,
+  node: konva.Node,
+  evtStr: K,
+  msg: (e: konva.KonvaEventObject<NodeEventMap[K]>) => UPDATE['_Msg'] | UPDATE['_Msg']
+): void {
+  let stream = kelmOrStream instanceof Kelm ? kelmOrStream.stream() : kelmOrStream
+  node.on(evtStr, e => {
+    let event = typeof msg === 'function' ? msg(e) : msg
+    stream(event)
+  })
 }
 
 export function create_node<NODE extends Node>(
